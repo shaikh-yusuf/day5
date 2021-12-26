7fd60b259398f23641bfe13209cddde03b05d021
shaikh-yusuf <shaikh8484047332@gmail.com>
2021-12-26 16:13:52 +0530

    updated

--- src/Component/Header/About.jsx
+++ src/Component/Header/About.jsx
@@ -4,6 +4,7 @@ export const About = () => {
     return (
         <div>
            <h1>About</h1>
+           <h2>muzakkir</h2>
            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/x0Y58wDhcIw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     );
